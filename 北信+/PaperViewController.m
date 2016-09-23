772f0260fd8058eb531383f3867f63f24e349d0d
windstormeye <877302410@qq.com>
2016-09-23 22:50:22 +0800

    完善试卷题目界面及当前试卷题目题号变化、搭建部分答题卡界面

--- 北信+/PaperViewController.m
+++ 北信+/PaperViewController.m
@@ -21,6 +21,11 @@
 @property (strong, nonatomic)  UILabel *nowLabel;
 @property (nonatomic, retain) NSMutableArray *numArr;
 @property (strong, nonatomic)  UILabel *timeLabel;
+@property (nonatomic, retain) NSMutableArray *titleArray;
+@property (nonatomic, retain) NSMutableArray *thingsArray;
+@property (nonatomic, assign) int number;
+
+
 
 
 @end
@@ -35,6 +40,24 @@
         _numArr = [[NSMutableArray alloc] init];
     }
     return _numArr;
+}
+
+-(NSArray *)titleArray
+{
+    if (!_titleArray)
+    {
+        _titleArray = [[NSMutableArray alloc] init];
+    }
+    return _titleArray;
+}
+
+-(NSMutableArray *)thingsArray
+{
+    if (!_thingsArray)
+    {
+        _thingsArray = [[NSMutableArray alloc] init];
+    }
+    return _thingsArray;
 }
 
 - (void)viewDidLoad {
@@ -73,7 +96,8 @@
 
 -(void)viewWillAppear:(BOOL)animated
 {
-    
+    self.number = 0;
+
     [super viewWillDisappear:animated];
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"close"] style:UIBarButtonItemStylePlain target:self action:@selector(back)];
@@ -84,8 +108,9 @@
     NSString *paperHttpUrl = @"http://cloud.bmob.cn/17f5e4c17ad52f4a/Get_Exams";
     [self requestPaper:paperHttpUrl];
     [self initPaperViewWithDict:self.paperDict];
-    
-    self.nowLabel.text = [NSString stringWithFormat:@"%d",0];
+    self.nowLabel.text = [NSString stringWithFormat:@"非题目"];
+    self.nowLabel.textColor = [UIColor grayColor];
+    self.nowLabel.font = [UIFont systemFontOfSize:18];
 }
 
 -(void)back
@@ -102,6 +127,8 @@
 
 - (void)initPaperViewWithDict:(NSDictionary *)dict
 {
+//    self.thingsArray = dict[@"Questions"];
+    
     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, self.navigationController.navigationBar.frame.size.height + 20, self.view.frame.size.width, 40)];
     view.backgroundColor = [UIColor whiteColor];
     UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, self.view.frame.size.width, view.frame.size.height - 1)];
@@ -121,35 +148,46 @@
     scrollView.backgroundColor = [UIColor whiteColor];
     self.scrollView = scrollView;
     
-    CGFloat maxX = self.scrollView.frame.size.width * arr.count;
-    self.scrollView.contentSize = CGSizeMake(maxX, 0);    // 设置scrollView的分页效果，是否分页，怎么分页，取决于ScrollView的宽度，系统默认以ScrollView的宽度为一页
+    CGFloat maxX = self.scrollView.frame.size.width * (arr.count - 1);
+    self.scrollView.contentSize = CGSizeMake(maxX, 0);
     self.scrollView.pagingEnabled = YES;
     self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.bounces = NO;
     
     CGFloat imgW = self.view.frame.size.width;
     CGFloat imgH = self.view.frame.size.height;
     CGFloat imgY = 0;
-    
+    CGFloat imgX = 0;
     int j = 0;
     for (int i = 1; i < arr.count; i ++)
     {
-        CGFloat imgX = (i-1) * imgW;
+        imgX = (i-1) * imgW;
         NSMutableString *str = dict[@"Questions"][0];
         // 区分大题目
         if ([self IsChinese:arr[i]])
         {
+            NSString *titleString = arr[i];
+            NSRange range = [titleString rangeOfString:@"|"];
+            titleString = [titleString substringToIndex:range.location];
+            [self.titleArray addObject:titleString];
+            
             UIView *chineseView = [[UIView alloc] initWithFrame:CGRectMake(imgX, imgY, imgW, imgH)];
             
-            UILabel *chineseLabel = [[UILabel alloc] init];
-            // 切记加下面这行代码，代表允许自动换行
-            chineseLabel.numberOfLines = 0;
+            UILabel *titleLabel = [[UILabel alloc] init];
             UIFont *font = [UIFont systemFontOfSize:10];
             CGSize textSize = [arr[i] sizeOfTextWithMaxSize:CGSizeMake(200, MAXFLOAT) font:font];
             CGFloat textW = textSize.width + imgW - 200;
             CGFloat textH = textSize.height + 30;
-            chineseLabel.frame = CGRectMake(5, imgW / 2, textW, textH);
-            chineseLabel.text = arr[i];
-            [chineseView addSubview:chineseLabel];
+            titleLabel.frame = CGRectMake(5, 20, 100, 30);
+            titleLabel.font = [UIFont systemFontOfSize:30];
+            titleLabel.textColor = [UIColor colorWithRed:26/255.0 green:167/255.0 blue:242/255.0 alpha:1.0];
+            titleLabel.text = titleString;
+            UILabel *textLabel = [[UILabel alloc] initWithFrame:CGRectMake(titleLabel.frame.origin.x, CGRectGetMaxY(titleLabel.frame), textW, textH)];
+            textLabel.text = [arr[i] substringFromIndex:range.location + 1];
+            // 切记加下面这行代码，代表允许自动换行
+            textLabel.numberOfLines = 0;
+            [chineseView addSubview:textLabel];
+            [chineseView addSubview:titleLabel];
             [scrollView addSubview:chineseView];
             
             NSNumber *num = [NSNumber numberWithInteger:i];
@@ -165,26 +203,42 @@
             
             UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(imgX, imgY, imgW, imgH)];
             NSURLRequest *request =[NSURLRequest requestWithURL:url];
-            webView.tag = j;
             [self.view addSubview: webView];
             [webView loadRequest:request];
             [scrollView addSubview:webView];
-            
+            NSNumber *num = [NSNumber numberWithInteger:i];
+            [self.thingsArray addObject:num];
             j++;
         }
     }
-        [self.view addSubview:scrollView];
+    UIView *answerView = [[UIView alloc] initWithFrame:CGRectMake(imgX, imgY, imgW, imgH)];
+    answerView.backgroundColor = [UIColor whiteColor];
+    
+    CGFloat labX = 10;
+    CGFloat labY = 5;
+    CGFloat labW = 50;
+    CGFloat labH = 30;
+    for (int i = 0; i < self.titleArray.count; i++)
+    {
+        UILabel *lab = [[UILabel alloc] initWithFrame:CGRectMake(labX, labY + (i * labH), labW, labH)];
+        lab.text = self.titleArray[i];
+        lab.font = [UIFont systemFontOfSize:14];
+        [answerView addSubview:lab];
+    }
+    
+    [scrollView addSubview:answerView];
+    [self.view addSubview:scrollView];
     
     UILabel *numSumlab = [[UILabel alloc] initWithFrame:CGRectMake(270, 0, 30, view.frame.size.height)];
     numSumlab.text = [NSString stringWithFormat:@"/ %d",j];
     [view addSubview:numSumlab];
     
-    UILabel *now = [[UILabel alloc] initWithFrame:CGRectMake(240, 0, 30, view.frame.size.height)];
+    UILabel *now = [[UILabel alloc] initWithFrame:CGRectMake(180, 0, 80, view.frame.size.height)];
     self.nowLabel = now;
     now.backgroundColor = [UIColor clearColor];
     now.textAlignment = NSTextAlignmentRight;
-    now.textColor = [UIColor colorWithRed:191/255.0 green:62/255.0 blue:255/255.0 alpha:1.0];
-    now.font = [UIFont systemFontOfSize: 24];
+    now.textColor = [UIColor colorWithRed:26/255.0 green:167/255.0 blue:242/255.0 alpha:1.0];
+    now.font = [UIFont systemFontOfSize: 20];
     [view addSubview:now];
     
     self.scrollView.delegate = self;
@@ -214,10 +268,8 @@
     for(int i=0; i< [str length];i++)
     {
         int a = [str characterAtIndex:i];
-        if( a > 0x4e00 && a < 0x9fff)
-        {
-            return YES;
-        }
+            if( a > 0x4e00 && a < 0x9fff)
+                return YES;
     }
     return NO;
 }
@@ -229,14 +281,26 @@
     // 当滑过一半的时候就跳到下一页
     offSetX = offSetX + (self.scrollView.frame.size.width / 2);
     int page = offSetX / self.scrollView.frame.size.width;
-    for (int m = 1; m<[self.numArr count]; m++)
+
+    NSNumber *num = [NSNumber numberWithInt:page + 1];
+//    NSNumber *num2 = [NSNumber numberWithInt:page];
+    if ([self.numArr containsObject:num])
     {
-        NSNumber *num = [self.numArr objectAtIndex:m];
-        if (page != [num integerValue])
-            self.nowLabel.text = [NSString stringWithFormat:@"%d",page];
+        self.nowLabel.text = [NSString stringWithFormat:@"非题目"];
+        self.nowLabel.textColor = [UIColor grayColor];
+        self.nowLabel.font = [UIFont systemFontOfSize:18];
+        self.number++;
     }
-    
-    
+    else if ([self.thingsArray containsObject:num])
+    {
+        self.nowLabel.text = [NSString stringWithFormat:@"%d",page];
+        self.nowLabel.textColor = [UIColor colorWithRed:26/255.0 green:167/255.0 blue:242/255.0 alpha:1.0];
+        self.nowLabel.font = [UIFont systemFontOfSize: 20];
+    }
+//    NSLog(@"%@",self.thingsArray);
+//    NSLog(@"%@ ---",self.numArr);
+//    NSLog(@"%d",self.number);
+
 }
 
 - (void)collect
