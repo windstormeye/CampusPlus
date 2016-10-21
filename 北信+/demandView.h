dea05ac7d1a6d32a12fbbbb7872a971cdc63b05b
windstormeye <877302410@qq.com>
2016-10-21 14:18:05 +0800

    发现模块活动发布界面及交互整体完成

---
+++ 北信+/demandView.h
@@ -0,0 +1,13 @@
+//
+//  demandView.h
+//  北信+
+//
+//  Created by #incloud on 16/10/20.
+//  Copyright © 2016年 #incloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface demandView : UIView
+
+@end
---
+++ 北信+/demandView.m
@@ -0,0 +1,21 @@
+//
+//  demandView.m
+//  北信+
+//
+//  Created by #incloud on 16/10/20.
+//  Copyright © 2016年 #incloud. All rights reserved.
+//
+
+#import "demandView.h"
+
+@implementation demandView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
