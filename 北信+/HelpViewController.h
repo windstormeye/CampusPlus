7fe6ad2dce04310b7a157df0b97fe12e6423a9a7
windstormeye <877302410@qq.com>
2016-11-02 22:16:26 +0800

    完成发布需求功能模块及下拉刷新

---
+++ 北信+/HelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelpViewController.h
+//  北信+
+//
+//  Created by #incloud on 16/11/2.
+//  Copyright © 2016年 #incloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelpViewController : UIViewController
+
+@end
